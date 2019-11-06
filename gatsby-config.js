64d6ae0586e614c05aca20e4b508ea30e8838224
Teo Oliver <teo.liz.oli@gmail.com>
2019-11-06 22:12:34 +0000

    fixed siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Website`,
+    description: `Teo Oliver personal/portifolio website`,
+    author: `Teo Oliver`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
