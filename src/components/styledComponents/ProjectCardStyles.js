632ad860a7cf14b2c126031567cb196129439d53
Teo Oliver <teo.liz.oli@gmail.com>
2020-02-22 23:06:33 +0000

    added card pickup effects on hover

--- src/components/styledComponents/ProjectCardStyles.js
+++ src/components/styledComponents/ProjectCardStyles.js
@@ -12,8 +12,13 @@ const ProjectCardStyles = styled.section`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   word-wrap: wrap break-word;
+  animation-name: pickup_effect_off;
+  animation-duration: 0.5s;
+  animation-fill-mode: forwards;
+  animation-timing-function: ease-in-out;
   :hover {
     box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    animation-name: pickup_effect_on;
   }
 
   hr {
@@ -64,6 +69,42 @@ const ProjectCardStyles = styled.section`
       border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
     border-radius: 0.25rem;
     text-decoration: none;
+  }
+
+  @keyframes pickup_effect_on {
+    0% {
+      transform: scale(1) rotateY(0deg) rotateX(0deg) rotateZ(0deg);
+    }
+    25% {
+      transform: scale(1) rotateY(20deg) rotateX(5deg) rotateZ(1deg);
+    }
+    50% {
+      transform: scale(1) rotateY(20deg) rotateX(5deg) rotateZ(2deg);
+    }
+    75% {
+      transform: scale(1.03) rotateY(0deg) rotateX(2.5deg) rotateZ(1deg);
+    }
+    100% {
+      transform: scale(1.03) rotateY(0deg) rotateX(0deg) rotateZ(0deg);
+    }
+  }
+
+  @keyframes pickup_effect_off {
+    0% {
+      transform: scale(1.03) rotateY(0deg) rotateX(0deg) rotateZ(0deg);
+    }
+    25% {
+      transform: scale(1.03) rotateY(0deg) rotateX(-2.5deg) rotateZ(-1deg);
+    }
+    50% {
+      transform: scale(1) rotateY(20deg) rotateX(-5deg) rotateZ(2deg);
+    }
+    75% {
+      transform: scale(1) rotateY(-20deg) rotateX(-5deg) rotateZ(1deg);
+    }
+    100% {
+      transform: scale(1) rotateY(0deg) rotateX(0deg) rotateZ(0deg);
+    }
   }
 `
 
