9ddf6f7d502e07d3b5db5c94f78b2019c3773e9c
Teo Oliver <teo.liz.oli@gmail.com>
2020-03-04 07:08:44 +0000

    theme vars scafolding

--- src/components/styledComponents/themeVars.js
+++ src/components/styledComponents/themeVars.js
@@ -1,6 +1,8 @@
 export const lightTheme = {
   backgroundColor: "#ffffff",
   primary: "#2e2e3d",
+  // title-background-color: ???
+
   // white
   // black
   // primary
@@ -11,3 +13,15 @@ export const darkTheme = {
   backgroundColor: "black",
   primary: "#2e2e3d",
 };
+
+// white = ???
+// black = ???
+// light-text = "white"
+// dark-text = "black"
+// lightcoral = "lightcoral" (instead of light-green)
+// background-primary = ???
+// background-secondary = ???
+// light-grey = #353b42
+// dark-gray = #232931
+// vivid-green = #52bc92
+// dark-green = #618382
