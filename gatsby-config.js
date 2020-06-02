23266e6a20eb766985fd5f0668331811223c8928
Teo Oliver <teo.liz.oli@gmail.com>
2020-06-02 20:00:35 +0100

    added custom favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/Letter_t.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon/favicon-16x16.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
