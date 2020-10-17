76502aecf37a46a4e1079e15b9ad3fbc05e55f33
Teo Oliver <teo.liz.oli@gmail.com>
2020-10-17 19:53:26 +0100

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import AboutStyles from "./styledComponents/AboutStyles";
 const About = () => {
   return (
     <AboutStyles>
-      <blockquote>
+      {/* <blockquote>
         Front-End developer with a passion for learning, collaboration and
         music.
       </blockquote>
@@ -17,7 +17,59 @@ const About = () => {
         After 10 years working in the music industry I decided to go fulltime as
         a programmer and leave music as a hobby. I love learning new
         technologies and languages and I'm always up for new challenges.
+      </p> */}
+
+      <h1>Hi there, I'm Teo 👋</h1>
+      <blockquote>
+        I'm a Web Developer aiming to be a React/Typescript specialist.
+      </blockquote>
+
+      <h2> About:</h2>
+      <ul>
+        <li>
+          {" "}
+          🌐 Github with examples of my work:{" "}
+          <a href="https://github.com/teooliver">
+            https://github.com/teooliver
+          </a>
+        </li>
+        <li>
+          {" "}
+          🌱 Learning more about and studying: **CS Algorithms, Testing, Figma,
+          Docker**
+        </li>
+        <li>
+          {" "}
+          😄 Passionate about accessibility and committed to a web that works
+          for everyone
+        </li>
+      </ul>
+
+      <h2>🚀 Side projects:</h2>
+      <ul>
+        <li>
+          <a href="https://habitgrid.xyz">HabitGrid</a> - A Habit Tracker PWA
+          built with React, Redux, Dexie.js and Sass.{" "}
+        </li>
+      </ul>
+
+      <h2>My Digital Garden 🌱</h2>
+      <p>
+        I write regular blog posts mostly about things I'm learning or would
+        like keep for future reference at
+        <a href="https://listed.to/@teooliver">listed.to/@teooliver</a>
       </p>
+
+      {/* Here are some ideas to get you started:
+
+- 🔭 I’m currently working on ...
+- 🌱 I’m currently learning ...
+- 👯 I’m looking to collaborate on ...
+- 🤔 I’m looking for help with ...
+- 💬 Ask me about ...
+- 📫 How to reach me: ...
+- 😄 Pronouns: ...
+- ⚡ Fun fact: ... */}
     </AboutStyles>
   );
 };
--- src/components/styledComponents/AboutStyles.js
+++ src/components/styledComponents/AboutStyles.js
@@ -10,17 +10,21 @@ const AboutStyles = styled.div`
   blockquote {
     border-left: 5px solid ${props => props.theme.aboutTextColor};
     padding-left: 1.5rem;
-    font-size: 2.2rem;
+    font-size: 1.8rem;
     margin-left: 0;
     margin-bottom: 2rem;
-    font-weight: 700;
+    font-weight: 400;
   }
-  p {
+  p,
+  li {
     font-weight: 300;
-    font-size: 1.68rem;
-    line-height: 2.88rem;
+    font-size: 1.2rem;
+    line-height: 2rem;
     margin-top: 0;
     margin-bottom: 1rem;
+    a {
+      padding-left: 0.2rem;
+    }
   }
 `;
 
